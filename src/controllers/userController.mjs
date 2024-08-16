f08cadea5cbade735e30df4630f97fc8b56ad562
Rhys18751996 <18751996@stduent.westernsydney.edu.au>
2024-08-16 22:00:00 +1000

    asd

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,11 +1,12 @@
 
 
-export function indexx(req, res) {
+export function index(req, res) {
     console.log("userController indexx fired");
     res.render("user/");
 }
 
 export function getUser(req, res) {
+    console.log("the user/user route was called");
     // Simulate fetching user data from a database
     const userId = req.params.id;
     const user = { id: userId, name: 'John Doe', email: 'john.doe@example.com' };
--- src/routes/web/userRoutes.mjs
+++ src/routes/web/userRoutes.mjs
@@ -1,18 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { indexx, getUser } from "../../controllers/userController.mjs";
-
+import { index, getUser } from "../../controllers/userController.mjs";
 
 router.get("/", (req, res) => {
-    console.log("userRoute fired");
-    res.render("user/");
-    //indexx(req, res)
+    index(req, res)
 });
 
-router.get("/user", (req, res) => {
-    console.log("the user/user route was called");
-    res.render("user/user");
+router.get("/getUser/:id", (req, res) => {
+    getUser(req, res);
 });
 
 // Define a route which goes to userController and uses its index method
